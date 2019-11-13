939ee1f35afc4cff81b2b2d8a0b4f496fbd78d1e
WonSuk Kim <wonsuk@wm4f4-94c.broadinstitute.org>
2019-11-13 15:39:23 -0500

    ws code added

---
+++ code_ws/mpc_multiply.cpp
@@ -0,0 +1,19 @@
+//
+// Created by WonSuk Kim on 11/13/19.
+//
+#include <fstream>
+//#include "connect.h"
+//#include "mpc.h"
+//#include "protocol.h"
+//#include "util.h"
+//#include "NTL/ZZ_p.h"
+#include <iostream>
+#include "mpc_multiply.h"
+using namespace std;
+
+
+
+int main() {
+    cout << "Read Matrix 1 ";
+    return 0;
+}
---
+++ code_ws/mpc_multiply.h
@@ -0,0 +1,14 @@
+//
+// Created by WonSuk Kim on 11/13/19.
+//
+
+#ifndef SECURE_GWAS_MPC_MULTIPLY_H
+#define SECURE_GWAS_MPC_MULTIPLY_H
+
+
+class mpc_multiply {
+
+};
+
+
+#endif //SECURE_GWAS_MPC_MULTIPLY_H
